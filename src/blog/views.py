b72b8ed31453fabab94a5e30c12f307944ae5cac
Brotot Biswas <brototi.biswas1@gmail.com>
2020-02-28 00:00:58 -0500

    add link on homepage to seasons blog

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Blog
+def blog_season_detail_view(request):
+    obj = Blog.objects.get(id=1)
+    context = {
+        'title' : obj.title,
+        'desc' : obj.desc
+    }
+    return render(request, "blog/seasons.html", context)
