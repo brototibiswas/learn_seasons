97e2ec1a35a1a02554ae84f18b3b5766d365b007
Brotot Biswas <brototi.biswas1@gmail.com>
2020-03-08 21:43:02 -0400

    simple django form with validation

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -11,6 +11,25 @@ class BlogpostForm(forms.ModelForm):
         ]
 
 
-class RawBlogpostForm(forms.Form):
-    title = forms.CharField()
-    desc = forms.CharField() 
+class RawSeasonPostForm(forms.Form):
+    CHOICES = [
+        ("WTR","Winter"), ("SMR","Summer"), ("FL","Fall"), ("SPR","Spring")
+    ]
+
+    season_name = forms.ChoiceField(
+        widget = forms.Select(
+            attrs = {
+                "class" : "form-control"
+            }
+        ), choices = CHOICES
+    )
+
+    season_desc = forms.CharField(
+        widget = forms.Textarea(
+            attrs = { 
+                    "class" : "form-control",
+                    "rows" : 6,
+                    "placeholder" : "Add few lines of Season description"
+            }
+        )
+    ) 
--- src/blog/views.py
+++ src/blog/views.py
@@ -2,15 +2,31 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import Season_Blog
-from .forms import BlogpostForm
+from .forms import RawSeasonPostForm
+
 
 def season_post_create_view(request):
-    if request.method == "POST":
-        name = request.POST.get('season_name')
-        print(name)
+    form = RawSeasonPostForm()
 
-    context = {}
+    if request.method == "POST":
+        form = RawSeasonPostForm(request.POST or None)
+        if form.is_valid():
+            print(form.cleaned_data)
+            Season_Blog.objects.create(**form.cleaned_data)
+
+    context = {
+        "form" : form
+    }
     return render(request, "blog/blogpost_create.html", context)
+
+
+# def season_post_create_view(request):
+#     if request.method == "POST":
+#         name = request.POST.get('season_name')
+#         print(name)
+
+#     context = {}
+#     return render(request, "blog/blogpost_create.html", context)
 
 
 def blog_desc_view(request):
